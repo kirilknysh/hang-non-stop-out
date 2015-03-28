c8bdee051f29ba4a16f326f85f957995eaf9d170
Andrei Yanovich <yanovich.andrei@gmail.com>
2015-03-28 13:44:09 +0300

    gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    maps = require('gulp-sourcemaps');
+
+
+var vendorScripts = [
+	'bower_components/leapjs/leap-0.6.2.js',
+	'bower_components/LeapTrainer.js/leaptrainer.js',
+	'bower_components/LeapTrainer.js/sub-classes/lib/brain.js',
+	'vendor/neural-networks.js',
+	'bower_components/three.js/three.js',
+	'bower_components/leapjs-plugins/main/leap-plugins-0.1.6.js',
+	'bower_components/leapjs-rigged-hand/build/leap.rigged-hand-0.1.4.js',
+	'bower_components/jquery/dist/jquery.js',
+	'bower_components/lodash/lodash.js'
+];
+
+var appScripts = [
+	'js/HNSOController.js',
+	'js/VisualController.js',
+	'js/main.js'
+];
 
 gulp.task('run-server', function (callback) {
     connect.server({
@@ -11,4 +35,38 @@ gulp.task('default', function () {
     //default task is to run the sever
 });
 
-//TODO: gulp build task has to create a fake auth/authConfig.js file
+gulp.task('build', function () {
+	runSequence('clean', 'vendor', 'app', 'styles', 'img');
+});
+
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
+});
+
+gulp.task('vendor', function () {
+	return gulp.src(vendorScripts)
+		.pipe(maps.init())
+	      .pipe(concat('vendor.min.js'))
+	      .pipe(uglify())
+	    .pipe(maps.write('.'))
+	    .pipe(gulp.dest('./build/vendor'));
+});
+
+gulp.task('app', function () {
+	return gulp.src(appScripts)
+		.pipe(maps.init())
+	      .pipe(concat('app.min.js'))
+	      .pipe(uglify())
+	    .pipe(maps.write('.'))
+	    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('styles', function () {
+	return gulp.src('css/*.css')
+		.pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('img', function () {
+	return gulp.src('img/*')
+		.pipe(gulp.dest('./build/img'));
+});
